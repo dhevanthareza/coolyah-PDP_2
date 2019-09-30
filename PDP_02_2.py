8a328a3ee71329e14271950272a42e9975f32af8
dhevanthareza <dhevanmuhamad@gmail.com>
2019-09-30 13:55:31 +0700

    sdnjkfnsak

--- PDP_02_2.py
+++ PDP_02_2.py
@@ -1,8 +1,10 @@
 """
+=======================================
 NAMA        : DHEVAN MUHAMAD ANTHAREZA
 NIM         : A11.2019.12293
 KELOMPOK    : A11.4118
 TANGGAL     : 23-9-2019 
+========================================
 """
 x = float(input("Masukan Nilai X : "))
 y = float(input("Masukan Nilai Y : "))
--- PDP_02_3.py
+++ PDP_02_3.py
@@ -1,8 +1,10 @@
 """
+=======================================
 NAMA        : DHEVAN MUHAMAD ANTHAREZA
 NIM         : A11.2019.12293
 KELOMPOK    : A11.4118
 TANGGAL     : 23-9-2019 
+========================================
 """
 a = float(input("Masukan Nilai A : "))
 t = float(input("Masukan Nilai T : "))
