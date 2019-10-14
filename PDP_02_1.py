dfb41409f1096cfdd4a114f22676b926aad9a39a
Galang Ekayudha Permana <galange99@gmail.com>
2019-10-14 17:05:22 +0700

    some changes

--- PDP_02_1.py
+++ PDP_02_1.py
@@ -6,6 +6,6 @@ KELOMPOK    : A11.4118
 TANGGAL     : 23-9-2019 
 ========================================
 """
-ayah='melati'
-ibu='mawar'
+ayah  ='melati'
+ibu   ='mawar'
 print("Nama Ayah : {}\nNama Ibu : {}".format(ayah,ibu))
